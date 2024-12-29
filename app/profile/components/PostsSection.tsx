869bddc61acff62d089ab82de201b1f3e8d57495
John Nguyen <john12052003@gmail.com>
2024-12-30 00:36:17 +0700

    landing page + profile page

--- app/profile/components/PostsSection.tsx
+++ app/profile/components/PostsSection.tsx
@@ -1,11 +1,28 @@
+import { ArrowRightIcon } from '@heroicons/react/solid';
 import React from 'react';
+
+import {Button} from '../ui';
 
 const PostsSection: React.FC = () => {
     return (
-        <div className='rounded-3xl bg-white p-6 shadow-md w-full'>
-            <h3 className='mb-4 text-lg font-bold'>Posts</h3>
-            <div className='text-gray-600'>
-                <p>No posts yet.</p>
+        <div className='flex w-full flex-col gap-4'>
+            <div className='flex flex-row gap-3'>
+                <Button variant='default'>Post</Button>
+                <Button variant='secondary'>Photos</Button>
+                <Button variant='secondary'>Videos</Button>
+                <Button variant='iconAfterShadow' size='default'>
+            More
+            <ArrowRightIcon className='h-4 w-4' />
+        </Button>
+            </div>
+
+            <div className='flex h-auto w-full flex-col rounded-3xl bg-white shadow-md'>
+                <div className='h-[50px] w-[200px] flex-shrink-0 border-b-2 border-[#2563EB] py-3 pl-6'>
+                    <h2 className='text-2xl font-bold text-[#2563EB]'>Posts</h2>
+                </div>
+                <div className='text-gray-600 p-6 '>
+                    <p>No posts yet.</p>
+                </div>
             </div>
         </div>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,8 +21,69 @@ const config: Config = {
                 desktop: '1440px',
             },
             colors: {
-                background: 'var(--background)',
-                foreground: 'var(--foreground)',
+                white: '#FCFCFC',
+                black: '#1E1E20',
+                primary: {
+                    DEFAULT: '#0369A1',
+                },
+                gray: {
+                    100: '#EFEFEF',
+                    200: '#DCDCDC',
+                    300: '#BDBDBD',
+                    400: '#989898',
+                    500: '#7C7C7C',
+                    600: '#656565',
+                    700: '#525252',
+                    800: '#464646',
+                    900: '#3D3D3D',
+                },
+                green: {
+                    100: '#DCFCE7',
+                    200: '#BBF7D0',
+                    300: '#86EFAC',
+                    400: '#4ADE80',
+                    500: '#22C55E',
+                    600: '#16A34A',
+                    700: '#15803D',
+                    800: '#166534',
+                    900: '#14532D',
+                },
+                orange: {
+                    100: '#FFEDD5',
+                    200: '#FED7AA',
+                    300: '#FDBA74',
+                    400: '#FB923C',
+                    500: '#F97316',
+                    600: '#EA580C',
+                    700: '#C2410C',
+                    800: '#9A3412',
+                    900: '#7C2D12',
+                },
+                red: {
+                    100: '#FEE2E2',
+                    200: '#FECACA',
+                    300: '#FCA5A5',
+                    400: '#F87171',
+                    500: '#EF4444',
+                    600: '#DC2626',
+                    700: '#B91C1C',
+                    800: '#991B1B',
+                    900: '#7F1D1D',
+                },
+                blue: {
+                    100: '#DBEAFE',
+                    200: '#BAE6FD',
+                    300: '#93C5FD',
+                    400: '#60A5FA',
+                    500: '#3B82F6',
+                    600: '#2563EB',
+                    700: '#1D4ED8',
+                    800: '#1E40AF',
+                    900: '#1E3A8A',
+                },
+            },
+            backgroundImage: {
+                'custom-gradient': 'linear-gradient(180deg, #2583FE, #F2C2FF, #FFF6A2)',
             },
             fontFamily: {
                 montserrat: ['Montserrat', 'sans-serif'],
@@ -45,13 +106,20 @@ const config: Config = {
                 display1: '6rem', // 96px
             },
             borderRadius: {
-                xs: '5px',
-                sm: '8px',
-                DEFAULT: '10px',
+                xs: '4px',
+                sm: '6px',
+                DEFAULT: '8px',
+                md: '10px',
                 lg: '12px',
                 xl: '16px',
             },
-        },
+            flex: {
+                2: '2 2 0%', // Grow 2 times compared to others
+            },
+            boxShadow: {
+                DEFAULT: '0 8px 25px 0 rgba(0, 0, 0, 0.25)'
+            }
+        }
     },
     plugins: [
         require('tailwindcss-animate'),
@@ -66,8 +134,24 @@ const config: Config = {
             addBase({
                 body: {
                     fontFamily: theme('fontFamily.montserrat'),
-                    fontSize: theme('fontSize.paragraph1'),
+                    // fontSize: theme('fontSize.paragraph1'),
+                    color: theme('colors.black'),
                 },
+                overline: { fontSize: theme('fontSize.overline') }, 
+                cap: { fontSize: theme('fontSize.caption') }, 
+                p2: { fontSize: theme('fontSize.paragraph2') }, 
+                p1: { fontSize: theme('fontSize.paragraph1') }, 
+                subtitle2: { fontSize: theme('fontSize.subtitle2') }, 
+                subtitle1: { fontSize: theme('fontSize.subtitle1') }, 
+                h6: { fontSize: theme('fontSize.heading6') }, 
+                h5: { fontSize: theme('fontSize.heading5') }, 
+                h4: { fontSize: theme('fontSize.heading4') }, 
+                h3: { fontSize: theme('fontSize.heading3') }, 
+                h2: { fontSize: theme('fontSize.heading2') }, 
+                h1: { fontSize: theme('fontSize.heading1') }, 
+                display3: { fontSize: theme('fontSize.display3') }, 
+                display2: { fontSize: theme('fontSize.display2') }, 
+                display1: { fontSize: theme('fontSize.display1') },
             });
         }),
     ],
